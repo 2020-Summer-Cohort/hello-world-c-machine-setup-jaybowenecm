edd47b2e47e81cba4ba036bde0e1c943bf8c5cf4
Jay Bowen <jay@bowenecmsolutions.com>
2020-05-12 11:03:40 -0400

    test two

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to day two.");
+            // This will 'call' the GetTheTime method
+            GetTheTime();
+        }
+
+
+
+        private static void GetTheTime()
+        {
+
+            String currentTime = DateTime.Now.ToShortTimeString();
+            Console.WriteLine("Hello World!");
+
+
         }
     }
 }
